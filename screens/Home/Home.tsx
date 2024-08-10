54f0fc467d82a5167deba5ec0c4a032513288abd
Kacper Gajewski <kacper.gajewski@britenet.com.pl>
2024-08-10 17:15:32 +0200

    - created call api function to get weather data by cords got before, WIP: implement types for BE response, and another app functionality

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,53 +1,127 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
+  Alert,
+  Button,
   SafeAreaView,
-  ScrollView,
   StatusBar,
+  StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
-import {
-  Colors,
-  Header,
-  LearnMoreLinks,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { getCityCords } from '../../services/weatherService';
 
 const Home = () => {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      const dupa = await getCityCords('Kielce');
-      console.log(dupa);
-    };
-    fetchWeatherData();
-  }, []);
+  const handleSearch = async () => {
+    if (city.trim() === '') {
+      Alert.alert('Input Error', 'Please enter a city name.');
+      return;
+    }
+
+    try {
+      const weatherData = await getCityCords('Kielce');
+      setWeather(weatherData);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      throw error;
+    }
+  };
+
+  // useEffect(() => {
+  //   const fetchWeatherData = async () => {
+  //     const dupa = await getCityCords('Kielce');
+  //     console.log(dupa);
+  //   };
+  //   fetchWeatherData();
+  // }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={{ ...backgroundStyle, ...styles.rootContainer }}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <LearnMoreLinks />
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Enter the name of a city below to fetch the current weather data.
+        </Text>
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter city name"
+            value={city}
+            onChangeText={setCity}
+          />
+          <Button title="Get Weather" onPress={handleSearch} color={'red'} />
         </View>
-      </ScrollView>
+        {weather && (
+          <View style={styles.weatherContainer}>
+            <Text style={styles.weatherText}>
+              Temperature: {weather.main.temp}°C
+            </Text>
+            <Text style={styles.weatherText}>
+              Condition: {weather.weather[0].description}
+            </Text>
+            <Text style={styles.weatherText}>
+              Humidity: {weather.main.humidity}%
+            </Text>
+            <Text style={styles.weatherText}>
+              Wind Speed: {weather.wind.speed} m/s
+            </Text>
+          </View>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  searchContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 24,
+    paddingHorizontal: 20,
+    color: '#000',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+    marginVertical: 20,
+    width: '100%',
+  },
+  weatherContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  weatherText: {
+    fontSize: 16,
+    marginVertical: 2,
+  },
+});
 
 export default Home;
--- services/weatherService.ts
+++ services/weatherService.ts
@@ -3,27 +3,18 @@ import { GetWeather } from '../types';
 import Config from 'react-native-config';
 
 const WEATHER_API_KEY = Config.WEATHER_API_KEY;
-const BASE_URL = process.env.BASE_URL;
 
 export const getCityCords = async (city: string) => {
-  // const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${WEATHER_API_KEY}`;
-  // const url = `http://api.openweathermap.org/geo/1.0/direct?q=Kielce&appid=${WEATHER_API_KEY}`;
-  // try {
-  //   console.log(WEATHER_API_KEY);
-  //   const response = await axios.get(url);
-  //   return response.data;
-  // } catch (error) {
-  //   console.error('Error fetching weather data:', error);
-  //   throw error;
-  // }
-  console.log(WEATHER_API_KEY);
-};
+  const getCordsUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${WEATHER_API_KEY}`;
 
-export const getWeather = async ({ city, lat, lon }: GetWeather) => {
-  const url = `${BASE_URL}?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`;
   try {
-    const response = await axios.get(url);
-    return response.data;
+    const cordsResponse = await axios.get(getCordsUrl);
+
+    const { lat, lon } = cordsResponse.data[0];
+    const getWeatherDataUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`;
+
+    const weatherResponse = await axios.get(getWeatherDataUrl);
+    return weatherResponse.data;
   } catch (error) {
     console.error('Error fetching weather data:', error);
     throw error;
